5a9f4260b5663a85ef3e19168b8af05973190aea
George Vigelette <gvigelet@vigeltek.com>
2015-03-09 20:15:26 -0500

    initial checkin

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfTrackerService;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            Uri serviceUri = new Uri("http://localhost:8000");
+            ServiceHost host = new ServiceHost(typeof(Service1), serviceUri);
+            host.AddServiceEndpoint(typeof(IService1), binding, "OperationService");
+            host.Open();
+
+            Console.WriteLine("Service is hosted to the Server");
+            Console.ReadLine();
+
+            host.Close();
+
+        }
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfTrackerService;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            ChannelFactory<IService1> factory = new ChannelFactory<IService1>(binding,
+        new EndpointAddress("http://localhost:8000/OperationService"));
+
+            IService1 proxy = factory.CreateChannel();
+            string methodFromServer = proxy.GetData(5);
+
+            Console.WriteLine(methodFromServer);
+
+            Console.ReadLine();
+        }
+    }
+}
