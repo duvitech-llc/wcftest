1f04a078adb5e60cfb3494062e03414a00212b15
George Vigelette <gvigelet@gmail.com>
2015-03-10 01:28:06 -0400

    working

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 using WcfTrackerService;
@@ -14,11 +16,17 @@ namespace ConsoleApplication3
         {
             BasicHttpBinding binding = new BasicHttpBinding();
 
-            Uri serviceUri = new Uri("http://localhost:8000");
+            Uri serviceUri = new Uri("http://localhost:8800");
             ServiceHost host = new ServiceHost(typeof(Service1), serviceUri);
             host.AddServiceEndpoint(typeof(IService1), binding, "OperationService");
+            
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(smb);
+            Binding mexBinding = MetadataExchangeBindings.CreateMexHttpBinding();
+            host.AddServiceEndpoint(typeof(IMetadataExchange), mexBinding, "mex");
             host.Open();
-
+            
             Console.WriteLine("Service is hosted to the Server");
             Console.ReadLine();
 
